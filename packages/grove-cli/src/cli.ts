3805d573a7ea793c50bcb027188a5aa0222e85f4
ferueda <fe.rueda@outlook.com>
2026-06-10 20:12:57 -0700

    feat(cli): add cli.ts

---
+++ packages/grove-cli/src/cli.ts
@@ -0,0 +1,20 @@
+import { Command } from "commander";
+import { acquireCmd } from "./commands/acquire.js";
+import { releaseCmd } from "./commands/release.js";
+import { statusCmd } from "./commands/status.js";
+import { destroyCmd, destroyAllCmd } from "./commands/destroy.js";
+
+const program = new Command();
+
+program
+  .name("grove")
+  .description("CLI for Grove - A programmatic git worktree pool manager")
+  .version("0.1.0");
+
+program.addCommand(acquireCmd);
+program.addCommand(releaseCmd);
+program.addCommand(statusCmd);
+program.addCommand(destroyCmd);
+program.addCommand(destroyAllCmd);
+
+await program.parseAsync(process.argv);
