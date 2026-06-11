94d717ade2acf96a625b126fa488314209c9f41e
ferueda <fe.rueda@outlook.com>
2026-06-11 16:07:27 -0700

    feat(cli): update cli.ts for lease mode

--- packages/grove-cli/src/cli.ts
+++ packages/grove-cli/src/cli.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import pc from "picocolors";
-import { handleError, setDebug } from "./error-handler.js";
+import { handleError, setDebug, setJson } from "./error-handler.js";
 import { acquireCmd } from "./commands/acquire.js";
 import { releaseCmd } from "./commands/release.js";
 import { statusCmd } from "./commands/status.js";
 import { destroyCmd, destroyAllCmd } from "./commands/destroy.js";
+import { inspectCmd } from "./commands/inspect.js";
+import { repairCmd } from "./commands/repair.js";
 
 process.on("uncaughtException", (err) => {
   console.error(pc.red(`Fatal: ${err.message}`));
@@ -24,9 +26,10 @@ program
   .description("CLI for Grove - A programmatic git worktree pool manager")
   .version("0.1.0")
   .option("--debug", "Show verbose error output including stack traces")
-  .hook("preAction", (thisCommand) => {
-    const opts = thisCommand.optsWithGlobals();
+  .hook("preAction", (thisCommand, actionCommand) => {
+    const opts = actionCommand.optsWithGlobals();
     if (opts.debug) setDebug(true);
+    if (opts.json) setJson(true);
   });
 
 program.addCommand(acquireCmd);
@@ -34,6 +37,8 @@ program.addCommand(releaseCmd);
 program.addCommand(statusCmd);
 program.addCommand(destroyCmd);
 program.addCommand(destroyAllCmd);
+program.addCommand(inspectCmd);
+program.addCommand(repairCmd);
 
 try {
   await program.parseAsync(process.argv);
